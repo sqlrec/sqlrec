a326d6b2515327fac9980c52471f5a4fc4a04078
46598 <465981796@qq.com>
2025-10-31 01:56:37 +0800

    support category diversify fun

---
+++ sqlrec-common/src/main/java/com/sqlrec/common/udf/table/CategoryDiversify.java
@@ -0,0 +1,109 @@
+package com.sqlrec.common.udf.table;
+
+import com.sqlrec.common.schema.CacheTable;
+import org.apache.calcite.linq4j.Enumerable;
+import org.apache.calcite.linq4j.Linq4j;
+import org.apache.calcite.rel.type.RelDataTypeField;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CategoryDiversify {
+    public CacheTable eval(
+            CacheTable input,
+            String categoryColumnName,
+            String windowSize,
+            String maxCategoryNumInWindow,
+            String maxReturnRecord
+    ) {
+        int windowSizeInt = Integer.parseInt(windowSize);
+        int maxCategoryNumInWindowInt = Integer.parseInt(maxCategoryNumInWindow);
+        int maxReturnRecordInt = Integer.parseInt(maxReturnRecord);
+
+        int categoryIndex = -1;
+        for (RelDataTypeField field : input.getDataFields()) {
+            if (field.getName().equals(categoryColumnName)) {
+                categoryIndex = field.getIndex();
+                break;
+            }
+        }
+        if (categoryIndex == -1) {
+            throw new IllegalArgumentException("catalogColumnName not found: " + categoryColumnName);
+        }
+
+        List<List<String>> categorys = new ArrayList<>();
+        List<Object[]> originData = new ArrayList<>();
+        Enumerable<Object[]> enumerable = input.scan(null);
+        for (Object[] data : enumerable) {
+            originData.add(data);
+            Object category = data[categoryIndex];
+            if (category == null) {
+                categorys.add(new ArrayList<>());
+            } else {
+                if (category instanceof List) {
+                    categorys.add(
+                            ((List<?>) category).stream()
+                                    .map(Object::toString)
+                                    .collect(Collectors.toList())
+                    );
+                } else {
+                    categorys.add(Collections.singletonList(category.toString()));
+                }
+            }
+        }
+
+        Set<Integer> usedIndex = new HashSet<>();
+        Map<String, Integer> categoryCount = new HashMap<>();
+        List<Object[]> newData = new ArrayList<>();
+        List<Integer> originRecordIndex = new ArrayList<>();
+        for (int i = 0; i < maxReturnRecordInt; i++) {
+            int validIndex = -1;
+            for (int j = 0; j < originData.size(); j++) {
+                if (usedIndex.contains(j)) {
+                    continue;
+                }
+                if (validIndex == -1) {
+                    validIndex = j;
+                }
+
+                boolean valid = true;
+                List<String> categories = categorys.get(j);
+                for (String category : categories) {
+                    if (categoryCount.getOrDefault(category, 0) >= maxCategoryNumInWindowInt) {
+                        valid = false;
+                        break;
+                    }
+                }
+
+                if (valid) {
+                    validIndex = j;
+                    break;
+                }
+            }
+
+            if (validIndex == -1) {
+                break;
+            }
+
+            newData.add(originData.get(validIndex));
+            originRecordIndex.add(validIndex);
+            for (String category : categorys.get(validIndex)) {
+                categoryCount.put(category, categoryCount.getOrDefault(category, 0) + 1);
+            }
+            usedIndex.add(validIndex);
+
+            if (i - windowSizeInt >= 0) {
+                List<String> categories = categorys.get(originRecordIndex.get(i - windowSizeInt));
+                for (String category : categories) {
+                    categoryCount.put(category, categoryCount.get(category) - 1);
+                }
+            }
+        }
+
+        return new CacheTable(
+                input.getTableName() + "_diversify",
+                Linq4j.asEnumerable(newData),
+                input.getDataFields()
+        );
+    }
+}
---
+++ sqlrec-core/src/test/java/com/sqlrec/TestCategoryDiversifyFunction.java
@@ -0,0 +1,98 @@
+package com.sqlrec;
+
+import com.sqlrec.common.schema.ExecuteContext;
+import com.sqlrec.common.schema.SqlRecTable;
+import com.sqlrec.common.udf.table.CategoryDiversify;
+import com.sqlrec.compiler.CompileManager;
+import com.sqlrec.compiler.NormalSqlCompiler;
+import com.sqlrec.runtime.BindableInterface;
+import com.sqlrec.schema.HmsSchema;
+import com.sqlrec.utils.JavaFunctionUtils;
+import org.apache.calcite.DataContext;
+import org.apache.calcite.jdbc.CalciteSchema;
+import org.apache.calcite.linq4j.Enumerable;
+import org.apache.calcite.linq4j.Linq4j;
+import org.apache.calcite.rel.type.RelDataType;
+import org.apache.calcite.rel.type.RelDataTypeFactory;
+import org.apache.calcite.schema.ScannableTable;
+import org.apache.calcite.schema.Table;
+import org.apache.calcite.schema.impl.AbstractSchema;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.type.SqlTypeName;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class TestCategoryDiversifyFunction {
+    @Test
+    public void testTableFunction() throws Exception {
+        ExecuteContext executeContext = new ExecuteContext();
+        CalciteSchema schema = CalciteSchema.createRootSchema(false);
+        schema.add(NormalSqlCompiler.DEFAULT_SCHEMA_NAME, new AbstractSchema() {
+            @Override
+            protected Map<String, Table> getTableMap() {
+                return Collections.singletonMap("myTable", new MyTable());
+            }
+        });
+        HmsSchema.setGlobalSchema(schema);
+        JavaFunctionUtils.registerTableFunction("default", "category_diversify", CategoryDiversify.class);
+
+        List<String> sqlList = Arrays.asList(
+                "cache table t1 as select * from myTable",
+                "cache table t2 as call category_diversify(t1, 'varchar_type', '2', '1', '10')",
+                "select * from t2",
+                "cache table t3 as call category_diversify(t1, 'array_varchar_type', '2', '1', '10')",
+                "select * from t3",
+                "cache table t4 as call category_diversify(t1, 'array_varchar_type', '3', '1', '10')",
+                "select * from t4",
+                "select id, UPPER(varchar_type) from t4",
+                "select id, UPPER(array_varchar_type[1]) from t4"
+        );
+
+        for (String sql : sqlList) {
+            System.out.println("\n" + sql);
+            SqlNode flinkSqlNode = CompileManager.parseFlinkSql(sql);
+            BindableInterface bindable = CompileManager.compileSql(flinkSqlNode, schema, NormalSqlCompiler.DEFAULT_SCHEMA_NAME);
+
+            Enumerable enumerable = bindable.bind(schema, executeContext);
+            if (enumerable != null) {
+                List<Object[]> results = enumerable.toList();
+                for (Object[] result : results) {
+                    System.out.println(java.util.Arrays.toString(result));
+                }
+                if (results.size() == 3) {
+                    assert results.get(0)[0].equals(1);
+                    assert results.get(1)[0].equals(3);
+                    assert results.get(2)[0].equals(2);
+                }
+            } else {
+                System.out.println("sql return null");
+            }
+        }
+    }
+
+    public static class MyTable extends SqlRecTable implements ScannableTable {
+
+        @Override
+        public @Nullable Enumerable<Object[]> scan(DataContext root) {
+            return Linq4j.asEnumerable(new Object[][]{
+                    {1, "a", Arrays.asList("a", "b")},
+                    {2, "a", Arrays.asList("a", "e")},
+                    {3, "b", Arrays.asList("d", "c")},
+            });
+        }
+
+        @Override
+        public RelDataType getRowType(RelDataTypeFactory typeFactory) {
+            return typeFactory.builder()
+                    .add("id", SqlTypeName.INTEGER)
+                    .add("varchar_type", SqlTypeName.VARCHAR)
+                    .add("array_varchar_type", typeFactory.createArrayType(typeFactory.createSqlType(SqlTypeName.VARCHAR), -1))
+                    .build();
+        }
+    }
+}
