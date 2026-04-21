6ec29cbc67d8430cfe3e42fa00b081c7a244697f
sqlrec <sqlrec@gmail.com>
2026-04-22 01:29:47 +0800

    update hdfs deploy

--- deploy/hdfs/deploy.sh
+++ deploy/hdfs/deploy.sh
@@ -5,11 +5,14 @@ source ~/.bash_profile
 dir=$(dirname $(realpath $0))
 source ${dir}/../env.sh
 
-if [ ! -e ${HDFS_NAMENODE_DATA_DIR} ]; then
-  mkdir -p ${HDFS_NAMENODE_DATA_DIR}
-  mkdir -p ${HDFS_DATANODE_DATA_DIR}
-  echo "export JAVA_HOME=${CLIENT_DIR}/${JAVA_CLIENT_DIR_NAME}" >> ${CLIENT_DIR}/${HADOOP_CLIENT_DIR_NAME}/etc/hadoop/hadoop-env.sh
-  ${CLIENT_DIR}/${HADOOP_CLIENT_DIR_NAME}/bin/hdfs namenode -format
+if ! kubectl get pvc ${HDFS_NAMENODE_PVC_NAME} -n "${NAMESPACE}" >/dev/null 2>&1; then
+  envsubst < ${dir}/hdfs-pvc.yaml > ${dir}/hdfs-pvc.yaml.tmp
+  kubectl apply -f ${dir}/hdfs-pvc.yaml.tmp -n "${NAMESPACE}"
+  
+  envsubst < ${dir}/hdfs-init-job.yaml > ${dir}/hdfs-init-job.yaml.tmp
+  kubectl apply -f ${dir}/hdfs-init-job.yaml.tmp -n "${NAMESPACE}"
+  
+  kubectl wait --for=condition=complete job/hdfs-namenode-init -n "${NAMESPACE}" --timeout=3600s
 fi
 
 envsubst < ${dir}/hdfs.yaml > ${dir}/hdfs.yaml.tmp
