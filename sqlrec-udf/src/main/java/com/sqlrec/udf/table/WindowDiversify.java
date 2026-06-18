38538dce77e589db1ee5901653d5ad686dd9bbcc
sqlrec <sqlrec@gmail.com>
2026-06-19 01:37:04 +0800

    optimize WindowDiversify udf

--- sqlrec-udf/src/main/java/com/sqlrec/udf/table/WindowDiversify.java
+++ sqlrec-udf/src/main/java/com/sqlrec/udf/table/WindowDiversify.java
@@ -1,12 +1,13 @@
 package com.sqlrec.udf.table;
 
 import com.sqlrec.common.schema.CacheTable;
-import org.apache.calcite.linq4j.Enumerable;
+import com.sqlrec.common.utils.DataTypeUtils;
 import org.apache.calcite.linq4j.Linq4j;
 import org.apache.calcite.rel.type.RelDataTypeField;
+import org.apache.calcite.sql.type.SqlTypeName;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WindowDiversify {
     public CacheTable evaluate(
@@ -31,79 +32,40 @@ public class WindowDiversify {
             throw new IllegalArgumentException("catalogColumnName not found: " + categoryColumnName);
         }
 
-        List<List<String>> categorys = new ArrayList<>();
-        List<Object[]> originData = new ArrayList<>();
-        Enumerable<Object[]> enumerable = input.scan(null);
-        for (Object[] data : enumerable) {
-            originData.add(data);
-            Object category = data[categoryIndex];
-            if (category == null) {
-                categorys.add(new ArrayList<>());
-            } else {
-                if (category instanceof List) {
-                    categorys.add(
-                            ((List<?>) category).stream()
-                                    .map(Object::toString)
-                                    .collect(Collectors.toList())
-                    );
-                } else {
-                    categorys.add(Collections.singletonList(category.toString()));
-                }
-            }
-        }
+        // Build rule table for RuleDiversity
+        int windowNum = Math.max(1, maxReturnRecordInt - windowSizeInt + 1);
 
-        Set<Integer> usedIndex = new HashSet<>();
-        Map<String, Integer> categoryCount = new HashMap<>();
-        List<Object[]> newData = new ArrayList<>();
-        List<Integer> originRecordIndex = new ArrayList<>();
-        for (int i = 0; i < maxReturnRecordInt; i++) {
-            int validIndex = -1;
-            for (int j = 0; j < originData.size(); j++) {
-                if (usedIndex.contains(j)) {
-                    continue;
-                }
-                if (validIndex == -1) {
-                    validIndex = j;
-                }
+        List<RelDataTypeField> ruleFields = new ArrayList<>();
+        ruleFields.add(DataTypeUtils.getRelDataTypeField("window_size", 0, SqlTypeName.INTEGER));
+        ruleFields.add(DataTypeUtils.getRelDataTypeField("window_start", 1, SqlTypeName.INTEGER));
+        ruleFields.add(DataTypeUtils.getRelDataTypeField("window_num", 2, SqlTypeName.INTEGER));
+        ruleFields.add(DataTypeUtils.getRelDataTypeField("diversity_column", 3, SqlTypeName.VARCHAR));
+        ruleFields.add(DataTypeUtils.getRelDataTypeField("diversity_value", 4, SqlTypeName.VARCHAR));
+        ruleFields.add(DataTypeUtils.getRelDataTypeField("op", 5, SqlTypeName.VARCHAR));
+        ruleFields.add(DataTypeUtils.getRelDataTypeField("diversity_num", 6, SqlTypeName.INTEGER));
+        ruleFields.add(DataTypeUtils.getRelDataTypeField("weight", 7, SqlTypeName.DOUBLE));
 
-                boolean valid = true;
-                List<String> categories = categorys.get(j);
-                for (String category : categories) {
-                    if (categoryCount.getOrDefault(category, 0) >= maxCategoryNumInWindowInt) {
-                        valid = false;
-                        break;
-                    }
-                }
+        Object[] ruleRow = new Object[]{
+                windowSizeInt,              // window_size
+                1,                          // window_start (1-based)
+                windowNum,                  // window_num (sliding)
+                categoryColumnName,         // diversity_column
+                null,                       // diversity_value (null = each distinct value)
+                "<",                        // op (count < maxCategoryNumInWindow)
+                maxCategoryNumInWindowInt,  // diversity_num
+                1.0                         // weight
+        };
 
-                if (valid) {
-                    validIndex = j;
-                    break;
-                }
-            }
+        List<Object[]> ruleRows = new ArrayList<>();
+        ruleRows.add(ruleRow);
 
-            if (validIndex == -1) {
-                break;
-            }
-
-            newData.add(originData.get(validIndex));
-            originRecordIndex.add(validIndex);
-            for (String category : categorys.get(validIndex)) {
-                categoryCount.put(category, categoryCount.getOrDefault(category, 0) + 1);
-            }
-            usedIndex.add(validIndex);
-
-            if (i - windowSizeInt >= 0) {
-                List<String> categories = categorys.get(originRecordIndex.get(i - windowSizeInt));
-                for (String category : categories) {
-                    categoryCount.put(category, categoryCount.get(category) - 1);
-                }
-            }
-        }
-
-        return new CacheTable(
-                input.getTableName() + "_diversify",
-                Linq4j.asEnumerable(newData),
-                input.getDataFields()
+        CacheTable ruleTable = new CacheTable(
+                "window_diversify_rule",
+                Linq4j.asEnumerable(ruleRows),
+                ruleFields
         );
+
+        RuleDiversity ruleDiversity = new RuleDiversity();
+        return ruleDiversity.evaluate(input, ruleTable, maxReturnRecord);
     }
 }
