0ffdd1dc7b053124c627316a493e1bf87a11660b
sqlrec <sqlrec@gmail.com>
2026-06-29 00:46:38 +0800

    optimize shell

--- bin/server.sh
+++ bin/server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
+if [ -n "${SQLREC_ENV_CONF}" ]; then
+  source "${SQLREC_ENV_CONF}"
+fi
+
 export LOG_DIR=${LOG_DIR:-/var/log/sqlrec}
 if [ -n "${HADOOP_HOME}" ]; then
   export PATH=$PATH:${HADOOP_HOME}/bin
@@ -9,4 +13,4 @@ if [ -n "${HADOOP_HOME}" ]; then
   export HADOOP_CONF_DIR=${HADOOP_CONF_DIR:-${HADOOP_HOME}/etc/hadoop}
 fi
 
-java -cp ./*:${CLASSPATH} com.sqlrec.frontend.Main
+exec java -cp ./*:${CLASSPATH} com.sqlrec.frontend.Main "$@"
